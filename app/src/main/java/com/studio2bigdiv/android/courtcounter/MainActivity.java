0f590c26fe5a3da3780363827039b353518c5c7b
Raphael Villas <raphael.villas@gmail.com>
2018-03-29 00:24:43 -0500

    Display points when buttons clicked

--- app/src/main/java/com/studio2bigdiv/android/courtcounter/MainActivity.java
+++ app/src/main/java/com/studio2bigdiv/android/courtcounter/MainActivity.java
@@ -2,8 +2,18 @@ package com.studio2bigdiv.android.courtcounter;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    /**
+     * Displays the given score for Team A.
+     */
+    public void displayForTeamA(View v) {
+        int score = Integer.parseInt(v.getTag().toString());
+        TextView scoreView = (TextView) findViewById(R.id.team_a_score);
+        scoreView.setText(String.valueOf(score));
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
