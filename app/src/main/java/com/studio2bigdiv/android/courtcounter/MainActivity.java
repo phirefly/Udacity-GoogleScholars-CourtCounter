0f5d5fd678eb6e76c69e6c41f3c547ee604ca5b8
Raphael Villas <raphael.villas@gmail.com>
2018-03-29 00:56:31 -0500

    Update the global score when clicking the buttons

--- app/src/main/java/com/studio2bigdiv/android/courtcounter/MainActivity.java
+++ app/src/main/java/com/studio2bigdiv/android/courtcounter/MainActivity.java
@@ -6,13 +6,16 @@ import android.view.View;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    int scoreTeamA = 0;
+
     /**
      * Displays the given score for Team A.
      */
     public void displayForTeamA(View v) {
         int score = Integer.parseInt(v.getTag().toString());
+        scoreTeamA = scoreTeamA + score;
         TextView scoreView = (TextView) findViewById(R.id.team_a_score);
-        scoreView.setText(String.valueOf(score));
+        scoreView.setText(String.valueOf(scoreTeamA));
     }
 
     @Override
